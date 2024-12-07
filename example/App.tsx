06cb2940fa3ec6a8c338f1abace00d92280e6da6
Kræn Hansen <mail@kraenhansen.dk>
2024-12-07 15:56:13 +0100

    WIP

--- example/App.tsx
+++ example/App.tsx
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { multiply } from 'react-native-automerge';
+
+const result = multiply(3, 7);
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>Result: {result}</Text>
     </View>
   );
 }
